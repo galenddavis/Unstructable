9af00ec07cbd20f7c275ae8cd298bdd6f70758fa
trash <73361653+galenddavis@users.noreply.github.com>
2021-01-20 09:48:08 -0500

    Create users migration and model

---
+++ db/migrate/20210120140029_create_users.rb
@@ -0,0 +1,17 @@
+class CreateUsers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :users do |t|
+      t.string :username, null: false
+      t.string :password_digest, null: false
+      t.string :session_token, null: false
+      t.string :occupation, null: false
+      t.string :email, null: false
+      t.string :location
+      t.text :user_bio
+      t.string :webstite
+
+      t.timestamps
+    end
+    add_index :users, :username
+  end
+end
